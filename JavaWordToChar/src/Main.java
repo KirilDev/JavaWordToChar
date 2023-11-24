33649ef3db8c7389ff1a6ca871b73fa8bfb5d2c1
Kirill_Dev <alexprogrammer2023@gmail.com>
2023-11-24 19:48:33 +0200

    upload files
    upload files

---
+++ JavaWordToChar/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String scanner = "Hello";
+        for (int i = 0; i < scanner.length(); i++)
+        {
+            System.out.println(scanner.charAt(i) + "\n");
+        }
+    }
+}
